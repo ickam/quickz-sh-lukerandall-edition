9815aef54497b72b140e837784e7c4db8a46a3f9
Msrek Mackiewicz <ickam89@gmail.com>
2020-08-29 19:58:16 +0100

    Update quickz.sh
    Remove remnants of old theme.

--- quickz.sh
+++ quickz.sh
@@ -53,22 +53,6 @@ else
 fi
 
 
-# INSTALL FONTS
-
-echo -e "Installing Nerd Fonts version of Hack, Roboto Mono, DejaVu Sans Mono\n"
-
-wget -q --show-progress -N https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf -P ~/.fonts/
-wget -q --show-progress -N https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf -P ~/.fonts/
-wget -q --show-progress -N https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete.ttf -P ~/.fonts/
-
-fc-cache -fv ~/.fonts
-
-if [ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]; then
-    cd ~/.oh-my-zsh/custom/themes/powerlevel10k && git pull
-else
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
-fi
-
 if [ -d ~/.quickzsh/fzf ]; then
     cd ~/.quickzsh/fzf && git pull
     ~/.quickzsh/fzf/install --all --key-bindings --completion --no-update-rc --64
