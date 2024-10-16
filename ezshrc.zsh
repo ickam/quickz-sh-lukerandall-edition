65df16de0d3b7f7225d4c24c469f0d80d84cb44b
Marek Mackiewicz <ickam89@gmail.com>
2024-10-16 19:27:33 +0100

    Update ezshrc.zsh
    cleanup

--- ezshrc.zsh
+++ ezshrc.zsh
@@ -9,6 +9,7 @@ export ZSH=$HOME/.config/ezsh/oh-my-zsh
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
 
+
 ZSH_THEME="lukerandall"
 
 
@@ -50,35 +51,21 @@ ZSH_THEME="lukerandall"
 # Would you like to use another custom folder than $ZSH/custom?
 # ZSH_CUSTOM=/path/to/new-custom-folder
 
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
 plugins=(
     zsh-completions
     zsh-autosuggestions
     zsh-syntax-highlighting
     history-substring-search
     screen
-    systemd
+    #systemd
     web-search
-    k
     extract
-    z
-    sudo
-    # httpie
-    # git
-    # python
-    # docker
-    # lol
-    # pip
-    # pyenv
-    # redis-cli
-    # zsh-wakatime          # enable if you use wakatime with 'https://github.com/wbingli/zsh-wakatime'
-    )
-#plugins+=(zsh-nvm)
+    encode64
+)
+  
+fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
 
-# source $ZSH/oh-my-zsh.sh          # This is now run in .zshrc after importing user configs from ~/.config/ezsh/zshrc/* files 
+source $ZSH/oh-my-zsh.sh
 
 # User configuration
 
@@ -87,87 +74,38 @@ plugins=(
 # You may need to manually set your language environment
 # export LANG=en_US.UTF-8
 
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
 
-# Compilation flags
-# export ARCHFLAGS="-arch x86_64"
-
-# ssh
-# export SSH_KEY_PATH="~/.ssh/rsa_id"
 
 # Set personal aliases, overriding those provided by oh-my-zsh libs,
 # plugins, and themes. Aliases can be placed here, though oh-my-zsh
 # users are encouraged to define aliases within the ZSH_CUSTOM folder.
 # For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
-
-# Add to PATH to Install and run programs with "pip install --user"
-export PATH=$PATH:~/.local/bin
-
-export PATH=$PATH:~/.config/ezsh/bin
-
-NPM_PACKAGES="${HOME}/.npm"
-PATH="$NPM_PACKAGES/bin:$PATH"
-
-[[ -s "$HOME/.config/ezsh/marker/marker.sh" ]] && source "$HOME/.config/ezsh/marker/marker.sh"
-
-autoload -U compinit && compinit -C -d ~/.cache/zsh/.zcompdump        # zsh-completions
-# autoload bashcompinit                 # bash completions
-# bashcompinit
-
-
-# QuickZsh
-SAVEHIST=50000      #save upto 50,000 lines in history. oh-my-zsh default is 10,000
-#setopt hist_ignore_all_dups     # dont record duplicated entries in history during a single session
 
 alias myip="wget -qO- https://wtfismyip.com/text"	# quickly show external ip address
 alias l="ls -lah"
-alias x="exit"
-
+alias x=
+alias taskman="ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head"
+alias subs="subliminal --opensubtitles ickam89@gmail.com 'zxNvEZL36VQ#Eq' 
+download -l en"
+alias napisy="subliminal --opensubtitles ickam89@gmail.com 
+'zxNvEZL36VQ#Eq' download -l pl"
+alias mdl="megatools dl"
+alias xcopy="rsync --bwlimit=15360"
+alias bat="pmset -g batt" #macOS only
+alias update="softwareupdate --all --install -R" 
+alias audiox='yt-dlp -x --audio-format mp3 "$1"'
+alias jdhead='java -Djava.awt.headless=true -jar JDownloader.jar'
 
 # CUSTOM FUNCTIONS
-
 # cheat sheets (github.com/chubin/cheat.sh), find out how to use commands
 # example 'cheat tar'
 # for language specific question supply 2 args first for language, second as the question
-# eample: cheat python3 execute external program
+# example: cheat python3 execute external program
 cheat() {
     if [ "$2" ]; then
         curl "https://cheat.sh/$1/$2+$3+$4+$5+$6+$7+$8+$9+$10"
     else
         curl "https://cheat.sh/$1"
-    fi
-}
-
-
-# Matrix screen saver! will run if you have installed "cmatrix"
-# TMOUT=900
-# TRAPALRM() { if command -v cmatrix &> /dev/null; then cmatrix -sb; fi }
-
-speedtest() {
-    curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -
-}
-
-dadjoke() {
-    curl https://icanhazdadjoke.com
-}
-
-# Find dictionary definition
-dict() {
-    if [ "$3" ]; then
-        curl "dict://dict.org/d:$1 $2 $3"
-    elif [ "$2" ]; then
-        curl "dict://dict.org/d:$1 $2"
-    else
-        curl "dict://dict.org/d:$1"
     fi
 }
 
@@ -177,19 +115,47 @@ ipgeo() {
    curl "http://ipinfo.io/"
 }
 
-# Show covid-19 spread stats
-corona() {
-    # Specify country otherwise shows stats for all
-    if [ "$1" ]; then
-        curl "https://corona-stats.online/$1"
-    else
-        curl "https://corona-stats.online"
-    fi
-}
-
 rand100() {
 curl https://www.random.org/integers/\?num\=1\&min\=1\&max\=100\&col\=1\&base\=10\&format\=plain\&rnd\=new &&
 }
 rand4() {
-curl  https://www.random.org/integers/\?num\=10\&min\=1\&max\=4\&col\=1\&base\=10\&format\=plain\&rnd\=new
+curl  
+https://www.random.org/integers/\?num\=10\&min\=1\&max\=4\&col\=1\&base\=10\&format\=plain\&rnd\=new
+}
+randp() {
+curl https://www.random.org/integers/\?num\=1\&min\=69\&max\=65000\&col\=1\&base\=10\&format\=plain\&rnd\=new &&
+}
+
+
+batchremux() {
+for f in *.mp4; do ffmpeg -i "$f" -c copy "${f%.mp4}.mkv"; done
+}
+sjpg() {
+ffmpeg \
+-ss 00:11:19 -i "$1" \
+-ss 00:21:36 -i "$1" \
+-ss 00:33:05 -i "$1" \
+-ss 00:45:51 -i "$1" \
+-map 0:v -vframes 1 output_01.jpg \
+-map 1:v -vframes 1 output_02.jpg \
+-map 2:v -vframes 1 output_03.jpg \
+-map 3:v -vframes 1 output_04.jpg
+}
+spng() {
+ffmpeg \
+-ss 00:11:19 -i "$1" \
+-ss 00:21:36 -i "$1" \
+-ss 00:33:05 -i "$1" \
+-ss 00:45:51 -i "$1" \
+-map 0:v -vframes 1 output_01.png \
+-map 1:v -vframes 1 output_02.png \
+-map 2:v -vframes 1 output_03.png \
+-map 3:v -vframes 1 output_04.png
+}
+convert() {
+ebook-convert "$1" "$2" --remove-paragraph-spacing
+}
+
+bulkrm() {
+find . -name "$1" -type f -delete
 }
